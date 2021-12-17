0fbb4280b39a870744575bfcea392828241f7c50
SeongMin-K <eo1tks3@naver.com>
2021-12-17 15:15:42 +0900

    Initial Commit

---
+++ Redux_Tutorial_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Redux_Tutorial_SwiftUI
+//
+//  Created by SeongMinK on 2021/12/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Redux_Tutorial_SwiftUI/Redux_Tutorial_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Redux_Tutorial_SwiftUIApp.swift
+//  Redux_Tutorial_SwiftUI
+//
+//  Created by SeongMinK on 2021/12/17.
+//
+
+import SwiftUI
+
+@main
+struct Redux_Tutorial_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
